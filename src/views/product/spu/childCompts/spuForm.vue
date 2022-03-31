17227284a40da471f1b8bf8e479ab09491650ab1
boleroDance <veiiki@qq.com>
2022-03-31 20:52:24 +0800

    更新spu

--- src/views/product/spu/childCompts/spuForm.vue
+++ src/views/product/spu/childCompts/spuForm.vue
@@ -23,13 +23,16 @@
         ></el-input>
       </el-form-item>
       <el-form-item label="SPU图片">
-        <!-- action：图片上传地址 -->
+        <!-- action：图片上传地址 
+        file-list是给照片墙做展示的数组 数组里必须要有name url属性
+         -->
         <el-upload
           action="/dev-api/admin/product/fileUpload"
           list-type="picture-card"
           :on-preview="handlePictureCardPreview"
           :on-remove="handleRemove"
           :file-list="spuImageList"
+          :on-success="handlerSuccess"
         >
           <i class="el-icon-plus"></i>
         </el-upload>
@@ -38,10 +41,25 @@
         </el-dialog>
       </el-form-item>
       <el-form-item label="销售属性">
-        <el-select placeholder="">
-          <el-option></el-option>
+        <el-select
+          :placeholder="`还有${unselectedAttr.length}项未选择`"
+          v-model="newAttr"
+        >
+          <el-option
+            :label="item.name"
+            :value="item"
+            v-for="(item, index) in unselectedAttr"
+            :key="item.id"
+          ></el-option>
         </el-select>
-        <el-button type="primary" icon="el-icon-plus">添加销售属性</el-button>
+        <el-button
+          type="primary"
+          :disabled="!newAttr"
+          @click="addSaleAttr"
+          icon="el-icon-plus"
+          style="margin-left: 10px"
+          >添加销售属性</el-button
+        >
         <!-- 展示当前spu属于自己的销售属性 -->
         <el-table style="width: 100%" border :data="spu.spuSaleAttrList">
           <el-table-column
@@ -50,17 +68,36 @@
             width="80px"
             align="center"
           ></el-table-column>
-          <el-table-column prop="saleAttrName" label="属性名" width="width"></el-table-column>
-          <el-table-column prop="" label="属性值列表" width="width">
-            <template slot-scope="row, $index">
+          <el-table-column
+            prop="saleAttrName"
+            label="属性名"
+            width="100"
+            align="center"
+          ></el-table-column>
+          <el-table-column prop="" label="属性值列表" width="580">
+            <template slot-scope="{ row, $index }">
               <el-tag
-              :key="tag.id"
-              v-for="(tag, index) in row.spuSaleAttrValueList"
-              closable
-              :disable-transitions="false"
-              >{{ tag.saleAttrValueName }}</el-tag>
-              <el-input class="input-new-tag" size="small"></el-input>
-              <el-button size="small">添加</el-button>
+                :key="tag.id"
+                v-for="(tag, index) in row.spuSaleAttrValueList"
+                closable
+                :disable-transitions="false"
+                @close="handleClose(tag)"
+                >{{ tag.saleAttrValueName }}</el-tag
+              >
+              <el-input
+                class="input-new-tag"
+                size="small"
+                v-if="row.inputVisible"
+                v-model="row.inputValue"
+                @blur="handleInputConfirm(row)"
+              ></el-input>
+              <el-button
+                @click="addSaleAttrValue(row)"
+                class="button-new-tag"
+                size="small"
+                v-else
+                >添加</el-button
+              >
             </template>
           </el-table-column>
           <el-table-column label="操作" width="width">
@@ -122,31 +159,71 @@ export default {
           // },
         ],
       },
+      // spuImageList 中我们将imgName属性改成了name... 服务器不需要，所以暂时放这
       spuImageList: [],
       trademarkList: [],
       //销售属性
       saleAttrList: [],
+      //收集未选择的销售属性id
+      newAttr: {},
     };
   },
   methods: {
     handleRemove(file, fileList) {
       //file参数:代表的是删除的那个张图片
       //fileList:照片墙删除某一张图片以后，剩余的其他的图片
-      // console.log(file, fileList,22222);
       //收集照片墙图片的数据
-      //对于已有的图片【照片钱中显示的图片：有name、url字段的】，因为照片墙显示数据务必要有这两个属性
+      //对于已有的图片【照片墙中显示的图片：有name、url字段的】，因为照片墙显示数据务必要有这两个属性
       //对于服务器而言，不需要name、url字段，将来对于有的图片的数据在提交给服务器的时候，需要处理的
       this.spuImageList = fileList;
     },
-    //照片墙图片预览的回调
     handlePictureCardPreview(file) {
+      //照片墙图片预览的回调
       //将图片地址赋值给这个属性
       this.dialogImageUrl = file.url;
       //对话框显示
       this.dialogVisible = true;
     },
+    handlerSuccess(response, file, fileList) {
+      // 照片墙新增图片后的回调
+      // console.log(response, file, fileList)
+      this.spuImageList = fileList;
+    },
+    addSaleAttr() {
+      // 添加销售属性
+      // 解构属性 修改属性名以便table展示
+      // 向spuSaleAttrList里添加新的销售属性
+      const { id: baseSaleAttrId, name: saleAttrName } = this.newAttr;
+      let newSaleAttr = {
+        baseSaleAttrId,
+        saleAttrName,
+        spuSaleAttrValueList: [],
+      };
+      this.spu.spuSaleAttrList.push(newSaleAttr);
+      // 清空此时没用的newAttr
+      this.newAttr = "";
+    },
+    addSaleAttrValue(row) {
+      // 添加属性值
+      // row.inputVisible = true
+      // 不是响应式
+      this.$set(row, "inputVisible", true);
+      // v-model 收集到row中
+      this.$set(row, "inputValue", "");
+    },
+    handleInputConfirm(row) {
+      // 失去焦点事件 变成tag
+      const { baseSaleAttrId, inputValue} = row;
+      if (inputValue.trim() == "") {
+        this.$message.warning("属性值不能为空");
+        return;
+      }
+      console.log(row)
+      let newSaleAttrValue = { baseSaleAttrId, saleAttrValueName: inputValue};
+      row.spuSaleAttrValueList.push(newSaleAttrValue);
+      row.inputVisible = false;
+    },
     initSpuData(row) {
-      console.log(row);
       // 这里要发送四个请求
       // 1.getTradmarkList 2.获取全部销售属性(3) baseSaleAttrList
       // 3.获取某一个spu信息 getSpuById 4. 获取对应spu图片 spuImageList
@@ -168,26 +245,53 @@ export default {
             let res = this.$API.spu.reqSpuImageList(row.id);
             resolve(res);
           }).then((imageres) => {
-            let imagelist = imageres.data
-            imagelist.forEach(element => {
-              element.name = element.imgName
-              element.url = element.imgUrl
+            let imagelist = imageres.data;
+            imagelist.forEach((element) => {
+              element.name = element.imgName;
+              element.url = element.imgUrl;
             });
-            this.spuImageList = imagelist
+            this.spuImageList = imagelist;
             return new Promise((resolve, reject) => {
               // 获取全部平台的销售属性
               let res = this.$API.spu.reqbaseSaleAttrList();
               resolve(res);
             }).then((baseSaleAttrres) => {
-              this.saleAttrList = baseSaleAttrres.data
+              this.saleAttrList = baseSaleAttrres.data;
             });
           });
         });
       });
     },
   },
+  computed: {
+    unselectedAttr() {
+      // saleAttrList 颜色 尺寸 版本
+      // spu.spuSaleAttrList 尺寸 大小
+      let res = this.saleAttrList.filter((item) => {
+        return this.spu.spuSaleAttrList.every((item1) => {
+          return item.name !== item1.saleAttrName;
+        });
+      });
+      return res;
+    },
+  },
 };
 </script>
 
 <style>
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+}
 </style>
