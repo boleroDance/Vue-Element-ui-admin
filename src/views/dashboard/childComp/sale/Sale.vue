c2d1384fc807f78662d7adb7163e97babb61a594
boleroDance <veiiki@qq.com>
2022-05-17 19:59:10 +0800

    更新echarts数据可视化部分

--- src/views/dashboard/childComp/sale/Sale.vue
+++ src/views/dashboard/childComp/sale/Sale.vue
@@ -1,8 +1,8 @@
 <template>
   <el-card style="margin-top: 10px">
-    <el-tabs class="el-tab">
-      <el-tab-pane label="销售额" name="first"> 销售额的数据 </el-tab-pane>
-      <el-tab-pane label="访问量" name="second"> 访问量的数据 </el-tab-pane>
+    <el-tabs class="el-tab" v-model="activeName">
+      <el-tab-pane label="销售额" name="salesData"> 销售额的数据 </el-tab-pane>
+      <el-tab-pane label="访问量" name="heatsData"> 访问量的数据 </el-tab-pane>
       <div class="tab-right">
         <span>本日</span><span>本周</span><span>本月</span><span>本年</span>
         <el-date-picker
@@ -18,15 +18,68 @@
     </el-tabs>
     <div>
       <el-row :gutter="10">
-        <el-col :span="16">123</el-col>
-        <el-col :span="8">456</el-col>
+        <el-col :span="18">
+         <sale-charts></sale-charts>
+        </el-col>
+        <el-col :span="6">
+          <div class="sale-right">
+            <h3>门店销售额排名</h3>
+            <ul>
+              <li>
+                <span class="sale-right-icon">1</span>
+                <span class="sale-center-text">肯德基</span>
+                <span class="sale-right-number">323.324</span>
+              </li>
+              <li>
+                <span class="sale-right-icon">2</span>
+                <span class="sale-center-text">麦当劳</span>
+                <span class="sale-right-number">299.132</span>
+              </li>
+              <li>
+                <span class="sale-right-icon">3</span>
+                <span class="sale-center-text">必胜客</span>
+                <span class="sale-right-number">284.132</span>
+              </li>
+              <li>
+                <span>4</span>
+                <span class="sale-center-text">海底捞</span>
+                <span class="sale-right-number">264.678</span>
+              </li>
+              <li>
+                <span>5</span>
+                <span class="sale-center-text">西贝莜面村</span>
+                <span class="sale-right-number">223.022</span>
+              </li>
+              <li>
+                <span>6</span>
+                <span class="sale-center-text">汉堡王</span>
+                <span class="sale-right-number">201.531</span>
+              </li>
+              <li>
+                <span>7</span>
+                <span class="sale-center-text">真功夫</span>
+                <span class="sale-right-number">197.523</span>
+              </li>
+            </ul>
+          </div>
+        </el-col>
       </el-row>
     </div>
   </el-card>
 </template>
 
 <script>
-export default {};
+import SaleCharts from './SaleCharts.vue'
+export default {
+  components: {
+    SaleCharts
+  },
+  data() {
+    return {
+      activeName: 'salesData'
+    }
+  }
+};
 </script>
 
 <style>
@@ -46,5 +99,35 @@ export default {};
 }
 .tab-right span {
   margin: 0 16px;
+}
+.sale-right ul {
+  width: 100%;
+  height: 300px;
+  list-style: none;
+  padding-left: 0;
+}
+.sale-right ul li {
+  margin:6px 0;
+  height: 30px;
+  line-height: 30px;
+}
+.sale-right-icon {
+  width: 15px;
+  height: 15px;
+  line-height: 15px;
+  display:inline-block;
+  background-color: black;
+  border-radius: 50%;
+  color: white;
+  font-size: 12px;
+  text-align: center;
+  float: left;
+  margin-top: 6px;
+}
+.sale-right-number {
+  float: right;
+}
+.sale-center-text {
+  padding-left: 16px;
 }
 </style>
---
+++ src/views/dashboard/childComp/sale/SaleCharts.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="sale-charts" ref="charts"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name: "",
+  mounted() {
+    let mySaleChart = echarts.init(this.$refs.charts);
+    var option = {
+      title: {
+        text: "销售额趋势"
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+          axisTick: {
+            alignWithLabel: true,
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+        },
+      ],
+      series: [
+        {
+          name: "Direct",
+          type: "bar",
+          barWidth: "60%",
+          data: [10, 52, 200, 334, 390, 330, 220, 23, 123, 67, 134, 98],
+          color: 'yellowgreen'
+        },
+      ],
+    };
+
+    mySaleChart.setOption(option);
+  },
+};
+</script>
+
+<style scoped>
+#sale-charts {
+  width: 100%;
+  height: 300px;
+}
+</style>
